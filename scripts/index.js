a1be34b2c496ff5f1d615804dec6f84316952e0f
Chey <cheyprivado@gmail.com>
2020-11-26 22:38:37 -0200

    update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,15 @@
 function convert() {
-    let binaryNum = document.querySelector("#binary-number")
-    let result = document.querySelector("#result-number")
+  let binaryNum = document.querySelector("#binary-number");
+  let result = document.querySelector("#result-number");
 
-    if (!binaryNum.value) {
-        window.alert("Please, enter a binary number.")
-    } else if (binaryNum.value.match(/^[0-1]+$/g) === null) {
-        window.alert("Please, enter a binary number.")
-    } else {
-        let value = parseInt(binaryNum.value, 2)
+  if (!binaryNum.value) {
+    window.alert("Please, enter a binary number.");
+  } else if (binaryNum.value.match(/^[0-1]+$/g) === null) {
+    window.alert("Please, enter a binary number.");
+  } else {
+    let value = parseInt(binaryNum.value, 2);
 
-        result.value = value
-    }
-    binaryNum.value = ''
+    result.value = value;
+  }
+  binaryNum.value = "";
 }
